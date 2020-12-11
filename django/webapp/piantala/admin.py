4b5b82fd026a44865c444f4f84387793879a6dfd
donatodipasquale <55739764+dipa96@users.noreply.github.com>
2020-12-11 17:11:45 +0100

    django piantala root

---
+++ django/webapp/piantala/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from piantala.models import Farmer, Genre, Product, ProductInstance
+
+#admin.site.register(Product)
+#admin.site.register(Farmer)
+admin.site.register(Genre)
+#admin.site.register(ProductInstance)
+
+
+# Define the admin class
+class FarmerAdmin(admin.ModelAdmin):
+     list_display = ('last_name', 'first_name', 'farm_loc')
+
+# Register the admin class with the associated model
+admin.site.register(Farmer, FarmerAdmin)
+
+# Register the Admin classes for Product using the decorator
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'farmer', 'display_genre')
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(ProductInstance) 
+class ProductInstanceAdmin(admin.ModelAdmin):
+    list_display = ('product', 'status', 'id')
+    list_filter = ('status', 'product')
